4bf6fea2dee0cabc71a786e930e19a891f25cd32
Hayoon <hy97@sju.ac.kr>
2023-11-11 02:03:32 +0900

    Title: 최대 점프 횟수, Time: 108ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 231110/최대 점프 횟수/maximum-number-of-jumps.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new  InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] board = new int[n];
+        int[] dp = new int[n];
+
+        String[] s = br.readLine().split(" ");
+        for(int i = 0; i < n; i++) {
+            board[i] = Integer.parseInt(s[i]);
+        }
+        int max = 0;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < i; j++) {
+                if(board[j] == 0) break;
+                if(j + board[j] >= i) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            max = Math.max(max, dp[i]);
+        }
+        System.out.println(max);
+    }
+}

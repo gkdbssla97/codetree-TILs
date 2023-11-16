5c73c7afc68e63136726bc4d5764315b7ad8050c
Hayoon <hy97@sju.ac.kr>
2023-11-17 02:37:41 +0900

    Title: 알바로 부자 되기, Time: 241ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 231117/알바로 부자 되기/being-rich-by-working-part-time.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] board = new int[n][3];
+        int[] dp = new int[n];
+        for(int i = 0; i < n; i++) {
+            String[] _s = br.readLine().split(" ");
+            int s = Integer.parseInt(_s[0]);
+            int e = Integer.parseInt(_s[1]);
+            int p = Integer.parseInt(_s[2]);
+
+            board[i][0] = s;
+            board[i][1] = e;
+            board[i][2] = p;
+
+            dp[i] = p;
+        }
+        Arrays.sort(board, (o1, o2) -> {
+            if(o1[0] == o2[0]) {
+                Integer.compare(o1[1], o2[1]);
+            } return Integer.compare(o1[0], o2[0]);
+        });
+        int max = -1;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < i; j++) {
+               if(board[j][1] < board[i][0]) {
+                    dp[i] += dp[j];
+                    max = Math.max(max, dp[i]);
+               } 
+            }
+        }
+        System.out.println(Arrays.stream(dp).max().getAsInt());
+    }
+}

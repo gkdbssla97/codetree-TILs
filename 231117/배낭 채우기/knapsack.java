dc5629a1a77685a990060d569356879f6c9d1885
Hayoon <hy97@sju.ac.kr>
2023-11-17 21:18:16 +0900

    Title: 배낭 채우기, Time: 107ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 231117/배낭 채우기/knapsack.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int[][] board;
+    static int[][] dp;
+    static boolean[][] visited;
+    public static void main(String[] args) throws Exception {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().split(" ");
+        int n = Integer.parseInt(s[0]);
+        int m = Integer.parseInt(s[1]);
+        board = new int[n + 1][2];
+        // visited = new boolean[sum + 1][n + 1];
+        int sum = 0;
+        for(int i = 0; i < n; i++) {
+            s = br.readLine().split(" ");
+            int w = Integer.parseInt(s[0]);
+            int v = Integer.parseInt(s[1]);
+            board[i + 1][0] = w;
+            board[i + 1][1] = v;
+            sum += w;
+        }
+        dp = new int[n + 1][m + 1];
+        int max = -1;
+        for(int i = 1; i <= n; i++) {
+            for(int j = 0; j <= m; j++) {
+                if(j >= board[i][0]) {
+                    dp[i][j] = Math.max(dp[i - 1][j - board[i][0]] + board[i][1], dp[i - 1][j]);
+                }
+                max = Math.max(dp[i][j], max);
+            }
+            // System.out.println(Arrays.toString(dp));
+        }
+        System.out.println(max);
+    }
+}

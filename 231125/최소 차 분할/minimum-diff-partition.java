780221fa4024f3e773f2ad19db696a25c712b8a8
Hayoon <hy97@sju.ac.kr>
2023-11-25 01:09:18 +0900

    Title: 최소 차 분할, Time: 73ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 231125/최소 차 분할/minimum-diff-partition.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] board = new int[n + 1];
+        
+        String[] s = br.readLine().split(" ");
+        int sum = 0;
+        for(int i = 0; i < n; i++) {
+            board[i + 1] = Integer.parseInt(s[i]);
+            sum += board[i + 1];
+        }
+        int[][] dp = new int[n + 1][sum + 1];
+        // 10 - 2n
+        for(int i = 1; i <= n; i++) {
+            for(int j = 0; j <= sum; j++) {
+                if(j - board[i] >= 0) {
+                    dp[i][j] = Math.max(dp[i - 1][j - board[i]] + board[i], dp[i][j]);
+                } else dp[i][j] = dp[i - 1][j];
+            }
+        }
+        int res = Integer.MAX_VALUE;
+        for(int i = 0; i <= sum; i++) {
+            // for(int j = 0; j <= n; j++) {
+                res = Math.min(res, Math.abs(sum -  2 * dp[n][i]));
+            // }
+        }
+        // for(int i = 0; i <= n; i++) {
+        //     System.out.println(Arrays.toString(dp[i]));
+        // }
+        System.out.println(res);
+    }
+}

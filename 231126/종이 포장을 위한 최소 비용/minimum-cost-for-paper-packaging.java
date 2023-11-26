55502ae0483526dc8edd3b41a57ba12e7807f7d6
Hayoon <hy97@sju.ac.kr>
2023-11-26 16:59:51 +0900

    Title: 종이 포장을 위한 최소 비용, Time: 85ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 231126/종이 포장을 위한 최소 비용/minimum-cost-for-paper-packaging.java
+++ 231126/종이 포장을 위한 최소 비용/minimum-cost-for-paper-packaging.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -9,14 +10,27 @@ public class Main {
         int n = Integer.parseInt(s[0]);
         int m = Integer.parseInt(s[1]);
 
-        int[] stone = new int[n];
-        int[] box = new int[m];
-
+        int[] stone = new int[n + 1];
+        int[] box = new int[m + 1];
+        int[] dp = new int[n + 1];
         for(int i = 0; i < n; i++) {
-            stone[i] = Integer.parseInt(br.readLine());
+            stone[i + 1] = Integer.parseInt(br.readLine());
         }
+        Arrays.sort(stone);
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        // 1 2 4 8 11 12
         for(int i = 0; i < m; i++) {
-            box[i] = Integer.parseInt(br.readLine());
+            box[i + 1] = Integer.parseInt(br.readLine());
         }
+        dp[0] = 0;
+        stone[0] = 1;
+        for(int i = 1; i <= n; i++) {
+            for(int j = i; j >= 1; j--) {
+                dp[i] = Math.min(dp[i], dp[j - 1] + box[stone[i] - stone[i - 1] + 1]);
+            }
+            System.out.println(Arrays.toString(dp));
+        }
+
+        
     }
 }

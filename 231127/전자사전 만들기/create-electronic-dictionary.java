acf7f2bd4e8a1c0342bed0a789773f81e26328d0
Hayoon <hy97@sju.ac.kr>
2023-11-27 17:16:28 +0900

    Title: 전자사전 만들기, Time: 3008ms, Memory: 28MB, Status: Time Limit Exceed - CodeTree

--- 231127/전자사전 만들기/create-electronic-dictionary.java
+++ 231127/전자사전 만들기/create-electronic-dictionary.java
@@ -30,42 +30,73 @@ public class Main {
             //시작위치 검색
             int left = 0;
             int right = board.length - 1;
-            int midS = -1;
-            while(left <= right) {
-                midS = (left + right) / 2;
+            // int midS = -1;
+            // int start = -1;
+            // while(left <= right) {
+            //     midS = (left + right) / 2;
 
-                if(board[midS].startsWith(b)) {
-                    right = midS - 1;
+            //     if(board[midS].startsWith(b)) {
+            //         left = midS + 1;
+            //     } else {
+            //         right = midS - 1;
+            //     }
+            // }
+            // start = midS;
+            // System.out.println("start: " + start);
+            // //끝 위치 검색
+            // left = start;
+            // right = board.length -1;
+            // int midE = -1;
+            // while(left <= right) {
+            //     midE = (left + right) / 2;
+
+            //     if(board[midE].startsWith(b)) {
+            //         break;
+            //     } else {
+            //         left = midE + 1;
+            //     }
+            // }
+            // int end = midE;
+            int start = -1;
+            int end = -1;
+            while(left <= right) {
+                if(board[left].startsWith(b) && board[right].startsWith(b)) {
+                    start = left;
+                    end = right;
+                    break;
+                } else if(board[left].startsWith(b)) {
+                    start = left;
+                    right--;
+                } else if(board[right].startsWith(b)) {
+                    end = right;
+                    left++;
                 } else {
-                    left = midS + 1;
+                    right--;
+                    left++;
                 }
             }
-            int start = left;
-            
-            //끝 위치 검색
-            left = start;
-            right = board.length -1;
-            int midE = -1;
-            while(left <= right) {
-                midE = (left + right) / 2;
-
-                if(board[midE].startsWith(b)) {
-                    left = midE + 1;
-                } else {
-                    right = midE - 1;
-                }
-            }
-            int end = right;
-            if(start + a - 1 >= board.length) {
+            // if(b.equals("q")) System.out.println(start + " " + end);
+            if(start + a - 1 >= board.length || start + a - 1 > end) {
                 System.out.println(-1);
             } else if(index.get(board[start + a - 1]) != null) {
                 System.out.println(index.get(board[start + a - 1]));
             } else {
-                if(index.get(board[start + a - 1]) == null) {
+                // if(index.get(board[start + a - 1]) == null) {
                     System.out.println(-1);
-                }
+                // }
             }
         }
     }
 }
 // 이후 이를 줄이기 위한 방향으로는 일일이 모든 단어를 살펴보기 보다는, 주어진 타겟 단어로 시작하는 문자열의 시작 위치와 끝 위치를 각각 이진탐색을 이용
+/**
+5 3
+da
+aa
+ab
+ba
+bc
+2 a
+2 b
+1 d
+**/

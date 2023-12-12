cc7597ebe40dd47b55923cd2e277babc830277cf
Hayoon <hy97@sju.ac.kr>
2023-12-12 17:45:10 +0900

    Title: 두 수의 합, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231212/두 수의 합/sum-of-two-num.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().split(" ");
+
+        HashMap<Integer, Integer> map = new LinkedHashMap<>();
+
+        int n = Integer.parseInt(s[0]);
+        int k = Integer.parseInt(s[1]);
+        s = br.readLine().split(" ");
+
+        for(int i = 0; i < n; i++) {
+            int key = Integer.parseInt(s[i]);
+            map.put(key, 0);
+        }
+        boolean[] visited = new boolean[100_001];
+        int cnt = 0;
+        for(int i = 0; i < n; i++) {
+            int key = Integer.parseInt(s[i]);
+            if(map.containsKey(k - key) && !visited[key] && !visited[k - key]) {
+                cnt++;
+                visited[key] = true;
+                visited[k - key] = true;
+            }
+        }
+        System.out.println(cnt);
+    }
+}

dc485ac410452791caa0c3307512b175df68c694
Hayoon <hy97@sju.ac.kr>
2023-12-12 16:25:16 +0900

    Title: 숫자 등장 횟수, Time: 896ms, Memory: 42MB, Status: Wrong Answer - CodeTree

---
+++ 231212/숫자 등장 횟수/number-frequency.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // 여기에 코드를 작성해주세요.
+        HashMap<Integer, Integer> map = new LinkedHashMap<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().split(" ");
+        int n = Integer.parseInt(s[0]);
+        int m = Integer.parseInt(s[1]);
+
+        s = br.readLine().split(" ");
+        for(int i = 0; i < n; i++) {
+            int key = Integer.parseInt(s[i]);
+            map.put(key, map.getOrDefault(key, 0) + 1);
+        }
+        s = br.readLine().split(" ");
+        for(int i = 0; i < s.length; i++) {
+            int key = Integer.parseInt(s[i]);
+            if(map.containsKey(key)) System.out.print(map.get(key) + " ");
+            else System.out.println(0);
+        }
+    }
+}

a99522bddbcd9517cc29bd49780d1a88b2364719
Hayoon <hy97@sju.ac.kr>
2023-12-17 18:12:10 +0900

    Title: 서로 다른 k개의 문자, Time: 226ms, Memory: 12MB, Status: Passed - CodeTree

--- 231217/서로 다른 k개의 문자/k-distinct-characters.java
+++ 231217/서로 다른 k개의 문자/k-distinct-characters.java
@@ -10,46 +10,35 @@ public class Main {
         int k = Integer.parseInt(s[1]);
 
         int right = 0;
-        int left = 0;
         int n = words.length();
         int cnt = 0;
         int len = -1;
         HashMap<Character, Integer> map = new LinkedHashMap<>();
-        while(left <= right && right < n) {
-            char c = words.charAt(right);
-            if(!map.containsKey(c) && cnt < k) {
-                if(right == len) {
-                    len = Math.max(len, right - left - 1);
-                } else {
-                    len = Math.max(len, right - left);
+        for(int left = 0; left < n; left++) {
+            while(right < n) {
+                char c = words.charAt(right++);
+                map.put(c, map.getOrDefault(c, 0) + 1);
+                if(map.get(c) == 1) {
+                    cnt++;
                 }
-                right++;
-                cnt++;
-                map.put(c, 1);
-            } else if(cnt <= k && map.containsKey(c)) {
-                if(right == len) {
+                if(cnt > k) {
                     len = Math.max(len, right - left - 1);
-                } else {
-                    len = Math.max(len, right - left);
+                    break;
                 }
-                right++;
-            } else if(!map.containsKey(c) && cnt >= k) {
-                if(right == len) {
-                    len = Math.max(len, right - left - 1);
-                } else {
-                    len = Math.max(len, right - left);
-                }
-                map.remove(words.charAt(left));
-                left++;
-                cnt--;    
+                // System.out.println("l, r" + " " + left + " " + right);
             }
-            // System.out.println("l, r" + " " + left + " " + right);
+            if(right == n && len == -1) {
+                len = right - left;
+                // System.out.println(len);
+                break;
+            }
+            char l = words.charAt(left);
+            map.put(l, map.get(l) - 1);
+            if(map.get(l) == 0) {
+                cnt--;
+            }
         }
-        if(right == len) {
-            len = Math.max(len, right - left - 1);
-        } else {
-            len = Math.max(len, right - left);
-        }
+
         System.out.println(len);
     }
 }

9f88967314f347c8fc0e5cbe2968f7090313e344
Hayoon <hy97@sju.ac.kr>
2024-04-24 02:34:52 +0900

    Title: 겹치지 않게 선분 고르기 2, Time: 344ms, Memory: 15MB, Status: Passed - CodeTree

--- 240424/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.java
+++ 240424/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.java
@@ -14,15 +14,23 @@ public class Main {
             board[i + 1][0] = x1;
             board[i + 1][1] = x2;
         }
-
+        Arrays.sort(board, (o1, o2) -> {
+            if(o1[0] == o2[0]) {
+                return Integer.compare(o1[1], o2[1]);
+            }
+            return Integer.compare(o1[0], o2[0]);
+        });
         int[] dp = new int[n + 1];
-        for(int i = 0; i <= n; i++) {
+        Arrays.fill(dp, 1);
+        dp[0] = 0;
+        for(int i = 1; i <= n; i++) {
             for(int j = 0; j < i; j++) {
                 if(board[j][1] < board[i][0]) {
                     dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
             }
         }
+        // System.out.println(Arrays.toString(dp));
         System.out.println(Arrays.stream(dp).map(e -> e).max().getAsInt());
     }
 }

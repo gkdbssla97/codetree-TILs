e140ee1f5ddf65218cf442e158b1c7a86d639373
Hayoon <hy97@sju.ac.kr>
2024-04-24 18:35:23 +0900

    Title: 동전 거슬러주기, Time: 91ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240424/동전 거슬러주기/coin-change.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new 	InputStreamReader(System.in));
+        String[] s = br.readLine().split(" ");
+
+        int n = Integer.parseInt(s[0]);
+        int m = Integer.parseInt(s[1]);
+        int[] board = new int[n + 1];
+        int[] dp = new int[m + 1];
+        Arrays.fill(dp, 100_001);
+        s = br.readLine().split(" ");
+        for(int i = 0; i < n; i++) {
+            board[i + 1] = Integer.parseInt(s[i]);
+        }
+        dp[0] = 0;
+        for(int i = 0; i <= m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i - board[j] >= 0) {
+                    if(dp[i - board[j]] == 100_001) continue;
+                    dp[i] = Math.min(dp[i], dp[i - board[j]] + 1);
+                }
+            }
+        }
+        System.out.println(dp[m]);
+    }
+}

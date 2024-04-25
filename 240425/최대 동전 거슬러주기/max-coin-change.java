35a6a0649a0120c7f9e74aef5671b2b714672717
Hayoon <hy97@sju.ac.kr>
2024-04-25 21:42:14 +0900

    Title: 최대 동전 거슬러주기, Time: 85ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240425/최대 동전 거슬러주기/max-coin-change.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new 	InputStreamReader(System.in));
+        String[] s = br.readLine().split(" ");
+	    int n = Integer.parseInt(s[0]);
+        int m = Integer.parseInt(s[1]);
+        
+        int[] board = new int[n];
+        int[] dp = new int[m + 1];
+        s = br.readLine().split(" ");
+        for(int i = 0; i < n; i++) {
+            board[i] = Integer.parseInt(s[i]);
+        }
+        dp[0] = 0;
+        for(int i = 0; i <= m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i - board[j] >= 0) {
+                    dp[i] = Math.max(dp[i], dp[i - board[j]] + 1);
+                }
+            }
+        }
+        
+        int res = -1;
+        if(dp[m] != res) res = dp[m];
+        System.out.println(dp[m]);
+    }
+}
